f12f654c766279bba0b8be0f1b746d99a8ce1199
Karabo Angel Dinga <skyydinga@gmail.com>
2023-10-06 13:24:12 +0200

    3

---
+++ IWA3/challenge2/scripts2.js
@@ -0,0 +1,7 @@
+// scripts.js
+import {role as nwabisa} from "./IWA3/challenge2/nwabisa.js";
+import {role as johannes} from "./johannes.js";
+import {role as alex} from "./alex.js";
+
+
+console.log('Roles:', nwabisa, ",", johannes,",", alex)
